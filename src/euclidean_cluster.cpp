6262e7e2dce3804454d2f7b6f338cb8554603872
ymp-kakeru <yamashita.kakeru392@mail.kyutech.jp>
2018-12-07 19:48:57 +0900

    add euclidean_clustering

--- src/euclidean_cluster.cpp
+++ src/euclidean_cluster.cpp
@@ -12,5 +12,74 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
-#include <pcl/visualization/cloud_viewer.h>
 
+typedef pcl::PointXYZ PointT;
+
+class EuclideanClustering
+{
+public:
+  EuclideanClustering();
+
+private:
+  void euclidean_clustering(const sensor_msgs::PointCloud2ConstPtr& input);
+  ros::NodeHandle nh_;
+  ros::Subscriber map_sub;
+
+  pcl::PointCloud<PointT> cloud;
+  pcl::PointCloud<PointT> cloud_cluster;
+  pcl::EuclideanClusterExtraction<PointT> ec ;
+  pcl::search::KdTree<PointT> tree;
+
+  std::vector<pcl::PointIndices> cluster_indices;
+  std::vector<pcl::PointIndices>::const_iterator v
+  std::vector<int>::const_iterator pit;
+
+  double tolerance, min_cluster, max_cluster; //許容誤差 最小・最大クラスタサイズ
+};
+
+EuclideanClustering::EuclideanClustering():
+  tolerance{0.05},min_cluster{100},max_cluster{10000}
+{
+  nh_.getParam("/euclidean_clustering/tolerance", tolerance);
+  nh_.getParam("/euclidean_clustering/min_cluster", min_cluster);
+  nh_.getParam("/euclidean_clustering/max_cluster", max_cluster);
+  map_sub = nh_.subscribe("cloud_seg",1,euclidean_clustering);
+}
+
+void euclidean_clustering(const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  pcl::fromROSMsg (*inpu, cloud);
+  tree->setInputCloud (cloud);
+
+  ec.setClusterTolerance (tolerance);
+  ec.setMinClusterSize (min_cluster);
+  ec.setMaxClusterSize (max_cluster);
+  ec.setSearchMethod (tree);
+  ec.setInputCloud (cloud.makeShared());
+  ec.extract (cluster_indices);
+
+  int j = 0;
+  for (v = cluster_indices.begin(); v != cluster_indices.end(); ++v)
+  {
+    for(pit = v->indices.begin(); pit != v->indices.end(); ++pit)
+    {
+      cloud_cluster->points.push_buck (cloud->points[*pit]);
+      cloud_cluster->width = cloud_cluster->points.size();
+      cloud_cluster->height = 1;
+      cloud_cluster->is_dense = true;
+
+      std :: cout << "PointCloud representing the Cluster: " << cloud_cluster -> points . size () << " data points." << std :: endl ;
+      std :: stringstream ss ;
+      ss << "cloud_cluster_" << j << ".pcd" ;
+      writer . write < pcl :: PointXYZ > ( ss . str (), * cloud_cluster , false ); //*
+      j ++ ;
+    }
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  ros::init (int argc, char** argv);
+  EuclideanClustering euclidean_clustering;
+  ros::spin();
+}
--- src/plane_segment.cpp
+++ src/plane_segment.cpp
@@ -17,6 +17,8 @@
 #include <pcl/filters/voxel_grid.h>
 
 typedef pcl::PointXYZ PointT;
+#define debug() std::cout << __LINE__ << std::endl;
+
 
 class PCL_segmentation
 {
@@ -35,18 +37,20 @@ private:
   double threshold;
   ros::NodeHandle nh_;
   ros::Subscriber cloud_sub ;
-//  ros::Publishr map_pub;
+  ros::Publisher map_pub;
 };
 
 PCL_segmentation::PCL_segmentation():
   threshold{0.1}
 {
+  debug();
   cloud_sub = nh_.subscribe("/input_cloud",10,&PCL_segmentation::cloud_Cb,this);
-  //map_pub = nh_.advertise<>();
+  map_pub = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_seg", 10);
 }
 
 void PCL_segmentation::cloud_Cb(const sensor_msgs::PointCloud2ConstPtr& input)
 {
+  debug();
   //rosメッセージであるPointCloud2型からpclで使用される型への変換
   pcl::fromROSMsg (*input, PCL_segmentation::cloud);
   //voxelgridによるダウンサンプリング
@@ -56,14 +60,14 @@ void PCL_segmentation::cloud_Cb(const sensor_msgs::PointCloud2ConstPtr& input)
   sor.filter (cloud);
   //RANSACを用いて平面検出および平面除去
   planeDetect();
-//  planeRemove(&cloud, inliers, negative);
+  planeRemove();
 }
 
 void PCL_segmentation::planeDetect()
 {
   //inliers : 平面抽出で得た点群のインデックス inliers.indices[0]
   //coefficients : 三次元平面方程式の未知数 ax+by+cz+d
-
+  debug();
   pcl::SACSegmentation<PointT> seg;
   seg.setOptimizeCoefficients(true);
   seg.setInputCloud (cloud.makeShared());
@@ -71,7 +75,6 @@ void PCL_segmentation::planeDetect()
   seg.setMethodType(pcl::SAC_RANSAC);
   seg.setDistanceThreshold(threshold);
   seg.segment(inliers, coefficients); 
-
    if (inliers.indices.size () == 0)
   {
     PCL_ERROR ("Could not estimate a planar model for the given dataset.");
@@ -81,20 +84,38 @@ void PCL_segmentation::planeDetect()
                                      << coefficients.values[1] << "y"
                                      << coefficients.values[2] << "z"
                                      << coefficients.values[3] << std::endl ;
-}
-
-/*void PCL_segmentation::planeRemove()
-{
+  
+  sensor_msgs::PointCloud2 cloud_seg;
   pcl::ExtractIndices<PointT> extract ;
   extract.setInputCloud(cloud.makeShared()); //入力点群
-  extract.setIndices(inliers); //指定平面
+  extract.setIndices(boost::make_shared<const pcl::PointIndices> (inliers)); //指定平面
   extract.setNegative(true); //true:remove Plane , false:remove without Plane
   extract.filter(cloud); //出力点群
-}*/
+
+  pcl::toROSMsg(cloud,cloud_seg);
+  map_pub.publish (cloud_seg);
+}
+
+void PCL_segmentation::planeRemove()
+{
+/*  sensor_msgs::PointCloud2 cloud_seg;
+
+  pcl::ExtractIndices<PointT> extract ;
+  extract.setInputCloud(cloud.makeShared()); //入力点群
+  extract.setIndices(PCL_segmentation::inliers); //指定平面
+  extract.setNegative(true); //true:remove Plane , false:remove without Plane
+  extract.filter(cloud); //出力点群
+
+  pcl::toROSMsg(cloud,cloud_seg);
+  map_pub.publish (cloud_seg);
+*/
+}
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "plane_segment");
-  //PCL_segmentation pcl_segmentation;
+  debug();
+  PCL_segmentation pcl_segmentation;
+  debug();
   ros::spin();
 }
